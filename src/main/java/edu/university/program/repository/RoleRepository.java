69b389a1193b9934913c2bb542c1c6564b2636d1
Roman Sulymka <roma.sulimak@gmail.com>
2021-01-11 13:55:41 +0200

    added RoleRepository

---
+++ src/main/java/edu/university/program/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package edu.university.program.repository;
+
+import edu.university.program.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
