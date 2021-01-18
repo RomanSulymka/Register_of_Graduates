4cabcf7b37e9e8dfbd678af396764f03c437ebfd
Roman Sulymka <roma.sulimak@gmail.com>
2021-01-18 19:55:13 +0200

    added UserServiceImpl

---
+++ src/main/java/edu/university/program/service/impl/UserServiceImpl.java
@@ -0,0 +1,50 @@
+package edu.university.program.service.impl;
+
+import edu.university.program.model.User;
+import edu.university.program.repository.UserRepository;
+import edu.university.program.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User create(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User update(User user) {
+        User oldUser = readById(user.getId());
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User readById(long id) {
+        Optional<User> optional = userRepository.findById(id);
+        return optional.get();
+    }
+
+    @Override
+    public void delete(long id) {
+        User user = readById(id);
+        userRepository.delete(user);
+    }
+
+    @Override
+    public List<User> getAll() {
+        List<User> users = userRepository.findAll();
+        List<User> users1 = users.isEmpty() ? new ArrayList<>() : users;
+        return users1;
+    }
+}
