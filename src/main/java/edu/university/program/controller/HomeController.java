f9b310670cd59c13aa9756a3e3d6b2a4489547dc
Roman Sulymka <roma.sulimak@gmail.com>
2021-01-27 11:44:44 +0200

    add logger to HomeController

--- src/main/java/edu/university/program/controller/HomeController.java
+++ src/main/java/edu/university/program/controller/HomeController.java
@@ -1,13 +1,19 @@
 package edu.university.program.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    Logger log = LoggerFactory.getLogger(HomeController.class);
+
 
     @GetMapping({"/", "home"})
     public String home() {
+
+        log.info("Go to home controller");
         return "home";
     }
 }
