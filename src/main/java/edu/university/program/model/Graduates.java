b5354cc369780ceb435eb99f8fb001a543e14156
Roman Sulymka <roma.sulimak@gmail.com>
2021-01-28 23:33:19 +0200

    add new fields

--- src/main/java/edu/university/program/model/Graduates.java
+++ src/main/java/edu/university/program/model/Graduates.java
@@ -5,8 +5,6 @@ import org.springframework.beans.factory.annotation.Value;
 
 import javax.persistence.*;
 import javax.validation.constraints.Pattern;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.Objects;
 
 @Entity
@@ -23,26 +21,53 @@ public class Graduates{
     @Column(name = "last_name", nullable = false)
     private String lastName;
 
+    @Value(" ")
+    @Column(name = "middle_name", nullable = true)
+    private String middleName;
+
     @Column(name = "company", nullable = false)
     private String company;
 
     @Column(name = "position", nullable = false )
     private String position;
 
+    @Column(name = "email", nullable = true)
+    private String email;
+
+    @Column(name = "gender", nullable = true)
+    private String gender;
+
     @Pattern(regexp = "(19[789]\\d|20[01]\\d)", message = "1970-2019 years")
-    @Column(name = "graduation_year", nullable = true)
-    private String graduationYear;
+    @Column(name = "graduation_year_from_bachelor", nullable = true)
+    private String graduationYearFromBachelor;
+
+    @Value(" - ")
+    @Pattern(regexp = "(19[789]\\d|20[01]\\d)", message = "1970-2019 years")
+    @Column(name = "graduation_year_from_master", nullable = true)
+    private String graduationYearFromMaster;
+
+    @Value(" - ")
+    @Pattern(regexp = "(19[789]\\d|20[01]\\d)", message = "1970-2019 years")
+    @Column(name = "graduation_year_from_specialist", nullable = true)
+    private String graduationYearFromOrkSpecialist;
 
     @Column(nullable = true, length = 64)
     private String photos;
 
-
-    public Graduates(String firstName, String lastName, String company, String position, String graduationYear) {
+    public Graduates(long id, String firstName, String lastName, String middleName,
+                     String company, String position, String email, String gender, String graduationYearFromBachelor,
+                     String graduationYearFromMaster, String graduationYearFromOrkSpecialist) {
+        this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.middleName = middleName;
         this.company = company;
         this.position = position;
-        this.graduationYear = graduationYear;
+        this.email = email;
+        this.gender = gender;
+        this.graduationYearFromBachelor = graduationYearFromBachelor;
+        this.graduationYearFromMaster = graduationYearFromMaster;
+        this.graduationYearFromOrkSpecialist = graduationYearFromOrkSpecialist;
     }
 
     public Graduates() {
@@ -88,12 +113,52 @@ public class Graduates{
         this.position = position;
     }
 
-    public String getGraduationYear() {
-        return graduationYear;
+    public String getMiddleName() {
+        return middleName;
     }
 
-    public void setGraduationYear(String graduationYear) {
-        this.graduationYear = graduationYear;
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getGraduationYearFromBachelor() {
+        return graduationYearFromBachelor;
+    }
+
+    public void setGraduationYearFromBachelor(String graduationYearFromBachelor) {
+        this.graduationYearFromBachelor = graduationYearFromBachelor;
+    }
+
+    public String getGraduationYearFromMaster() {
+        return graduationYearFromMaster;
+    }
+
+    public void setGraduationYearFromMaster(String graduationYearFromMaster) {
+        this.graduationYearFromMaster = graduationYearFromMaster;
+    }
+
+    public String getGraduationYearFromOrkSpecialist() {
+        return graduationYearFromOrkSpecialist;
+    }
+
+    public void setGraduationYearFromOrkSpecialist(String graduationYearFromOrkSpecialist) {
+        this.graduationYearFromOrkSpecialist = graduationYearFromOrkSpecialist;
     }
 
     public String getPhotos() {
@@ -119,16 +184,21 @@ public class Graduates{
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Graduates graduates = (Graduates) o;
-        return graduationYear == graduates.graduationYear && Objects.equals(id, graduates.id)
+        return id == graduates.id
                 && Objects.equals(firstName, graduates.firstName)
-                && Objects.equals(lastName, graduates.lastName)
-                && Objects.equals(company, graduates.company)
-                && Objects.equals(position, graduates.position);
+                && Objects.equals(lastName, graduates.lastName) && Objects.equals(middleName, graduates.middleName)
+                && Objects.equals(company, graduates.company) && Objects.equals(position, graduates.position)
+                && Objects.equals(email, graduates.email) && Objects.equals(gender, graduates.gender)
+                && Objects.equals(graduationYearFromBachelor, graduates.graduationYearFromBachelor)
+                && Objects.equals(graduationYearFromMaster, graduates.graduationYearFromMaster)
+                && Objects.equals(graduationYearFromOrkSpecialist, graduates.graduationYearFromOrkSpecialist)
+                && Objects.equals(photos, graduates.photos);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, firstName, lastName, company, position, graduationYear);
+        return Objects.hash(id, firstName, lastName, middleName, company, position, email, gender,
+                graduationYearFromBachelor, graduationYearFromMaster, graduationYearFromOrkSpecialist, photos);
     }
 
     @Override
@@ -137,11 +207,14 @@ public class Graduates{
                 "id=" + id +
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
+                ", middleName='" + middleName + '\'' +
                 ", company='" + company + '\'' +
                 ", position='" + position + '\'' +
-                ", graduationYear='" + graduationYear + '\'' +
-                ", photos='" + photos + '\'' +
+                ", email='" + email + '\'' +
+                ", gender='" + gender + '\'' +
+                ", graduationYearFromBachelor='" + graduationYearFromBachelor + '\'' +
+                ", graduationYearFromMaster='" + graduationYearFromMaster + '\'' +
+                ", graduationYearFromOrkSpecialist='" + graduationYearFromOrkSpecialist + '\'' +
                 '}';
     }
-
 }
