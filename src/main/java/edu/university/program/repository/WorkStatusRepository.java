69f44b1b5bec42d269b58d2843ec35a8698b8885
Roman Sulymka <roma.sulimak@gmail.com>
2021-02-02 00:25:56 +0200

    add work status

--- src/main/java/edu/university/program/repository/WorkStatusRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.university.program.repository;
-
-import edu.university.program.model.WorkStatus;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WorkStatusRepository extends JpaRepository<WorkStatus, Long> {
-}
